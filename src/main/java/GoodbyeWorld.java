08fe8aca4f70bfd5ec1d5b5fece10095ae955f5d
Andrea Giordano - andrea.giordano5@studio.unibo.it <andrea.giordano5@studio.unibo.it>
2019-05-08 15:11:51 +0200

    is time

--- src/main/java/GoodbyeWorld.java
+++ src/main/java/GoodbyeWorld.java
@@ -1,5 +1,9 @@
 public class GoodbyeWorld{
 	public static void main(String... args){
-		System.out.println("Goodbye, World! My time has come!");
+		System.out.println("Goodbye, World! " + isTime());
+	}
+	
+	private static String isTime(){
+		return "My time has come!";
 	}
 }
